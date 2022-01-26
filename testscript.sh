6df2699a9452d244325be21be210164cb64674f2
trriplejay <johns@jfrog.com>
2022-01-26 10:32:29 -0800

    no xml, use output instead

--- testscript.sh
+++ testscript.sh
@@ -4,5 +4,5 @@
 echo "this script is executed in a subshell that inherits ENV from the parent"
 
 add_run_variables "foo=bar" "hello=world"
-save_tests ./*
+write_output settings_bag "date=$(date +%s)" "run_id=$run_id"
 echo "my token is $res_utilRepo_gitProvider_token"
